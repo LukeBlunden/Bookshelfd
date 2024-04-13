16a2f54b99efd0afbfb19870679f8a9eccf67aac
lukem <Lucas.Blunden@gmail.com>
2024-04-13 14:45:17 +0100

    Added column annotations, changed some property names and data types

--- src/main/java/com/example/Bookshelfd/book/Book.java
+++ src/main/java/com/example/Bookshelfd/book/Book.java
@@ -2,10 +2,10 @@ package com.example.Bookshelfd.book;
 
 import jakarta.persistence.*;
 
-import java.time.LocalDate;
+import java.util.Date;
 
-@Entity
-@Table
+@Entity(name = "Book")
+@Table(name = "book")
 public class Book {
     @Id
     @SequenceGenerator(
@@ -17,27 +17,35 @@ public class Book {
             strategy = GenerationType.SEQUENCE,
             generator = "book_sequence"
     )
+    @Column(name = "id", updatable = false)
     private long id;
-    private String name;
+    @Column(name = "title", nullable = false, columnDefinition = "TEXT")
+    private String title;
+    @Column(name = "title", nullable = false, columnDefinition = "TEXT")
     private String author;
+    @Column(name = "title", nullable = false, columnDefinition = "TEXT")
     private String cover;
+    @Column(name = "title", nullable = false, columnDefinition = "TEXT")
     private String description;
-    private Integer published;
+    @Column(name = "title", nullable = false)
+    private Date published;
+//    Probably should include ISBN, check API's for type
+
 
     public Book() {
     }
 
-    public Book(String name, String author, String cover, String description, Integer published) {
-        this.name = name;
+    public Book(String title, String author, String cover, String description, Date published) {
+        this.title = title;
         this.author = author;
         this.cover = cover;
         this.description = description;
         this.published = published;
     }
 
-    public Book(long id, String name, String author, String cover, String description, Integer published) {
+    public Book(long id, String title, String author, String cover, String description, Date published) {
         this.id = id;
-        this.name = name;
+        this.title = title;
         this.author = author;
         this.cover = cover;
         this.description = description;
@@ -52,12 +60,12 @@ public class Book {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getTitle() {
+        return title;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getAuthor() {
@@ -84,11 +92,11 @@ public class Book {
         this.description = description;
     }
 
-    public Integer getPublished() {
+    public Date getPublished() {
         return published;
     }
 
-    public void setPublished(Integer published) {
+    public void setPublished(Date published) {
         this.published = published;
     }
 
@@ -96,7 +104,7 @@ public class Book {
     public String toString() {
         return "book{" +
                 "id=" + id +
-                ", name='" + name + '\'' +
+                ", name='" + title + '\'' +
                 ", author='" + author + '\'' +
                 ", cover='" + cover + '\'' +
                 ", description='" + description + '\'' +
